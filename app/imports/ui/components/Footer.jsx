281585e7bf99c518155f812d1be955349f419152
balemayehu1 <97714392+balemayehu1@users.noreply.github.com>
2022-04-09 11:25:43 -1000

    footer updated

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Container, Menu, Icon } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
     return (
       <footer>
-        <div style={divStyle} className="ui center aligned container">
-          <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822 <br />
-          <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Template Home Page</a>
-        </div>
+        <Menu className="secondary">
+          <Container>
+            <Menu.Item className="left">AkaMy-Rent</Menu.Item>
+            <Menu.Item ><Icon className="twitter"/></Menu.Item>
+            <Menu.Item ><Icon className="facebook f"/></Menu.Item>
+            <Menu.Item className="right">Contact Us</Menu.Item>
+          </Container>
+        </Menu>
       </footer>
     );
   }
