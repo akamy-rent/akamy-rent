13ca7ba762aeeb902c4eb96c0ecf7e8028741d25
balemayehu1 <97714392+balemayehu1@users.noreply.github.com>
2022-04-09 11:29:27 -1000

    nav bar updated

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -12,8 +12,11 @@ class NavBar extends React.Component {
     const menuStyle = { marginBottom: '10px' };
     return (
       <Menu style={menuStyle} attached="top" borderless inverted>
+        <Menu.Item className="left">
+          About
+        </Menu.Item>
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
-          <Header inverted as='h1'>meteor-application-template</Header>
+          <Header inverted as='h1'>AkaMy-Rent</Header>
         </Menu.Item>
         {this.props.currentUser ? (
           [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
