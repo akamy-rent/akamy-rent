f3b5660bdf48f8624d98fc4196a22bc5bc0a107a
htobin <35468353+htobin@users.noreply.github.com>
2022-04-10 01:02:11 -1000

    Updated to look like tutorial

--- app/imports/startup/client/contracts/greeter.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity >= 0.4.0;
-
-contract Greeter {
-    function greet() external pure returns(string memory) {
-        return "Hello, World!";
-    }
-}
--- app/imports/startup/client/ethersFunctions.js
+++ app/imports/startup/client/ethersFunctions.js
@@ -5,35 +5,10 @@ const fs = require('fs');
 const solc = require('solc');
 
 // const nodeUrl = 'http://localhost:8545';
-export function printContract() {
-  const input = {
-    language: 'Solidity',
-    sources: {
-      'test.sol': {
-        content: 'contract C { function f() public { } }',
-      },
-    },
-    settings: {
-      outputSelection: {
-        '*': {
-          '*': ['*'],
-        },
-      },
-    },
-  };
+const greeterPath = path.resolve(__dirname, 'contracts', 'greeter.sol');
+const source = fs.readFileSync(greeterPath, 'UTF-8');
 
-  const output = JSON.parse(solc.compile(JSON.stringify(input)));
-
-  // `output` here contains the JSON output as specified in the documentation
-
-  for (const contractName in output.contracts['test.sol']) {
-    console.log(
-      `${contractName
-      }: ${
-        output.contracts['test.sol'][contractName].evm.bytecode.object}`,
-    );
-  }
-}
+console.log(solc.compile(source, 1));
 
 // A Provider is an abstraction of a connection to the Ethereum network, providing a concise, consistent interface to standard Ethereum node functionality.
 // const nodeConnection = new ethers.providers.JsonRpcProvider(nodeUrl);
