c9f285b835d919f773984d9ac1f2235fb37f9ee0
balemayehu1 <97714392+balemayehu1@users.noreply.github.com>
2022-04-12 16:11:09 -1000

    latest attempt

--- app/imports/ui/components/BottomHalf.jsx
+++ app/imports/ui/components/BottomHalf.jsx
@@ -7,12 +7,37 @@ class BottomHalf extends React.Component {
     const gridStyle = { height: '200px', textAlign: 'left' };
     return (
       <div className="landingBG">
-        <Grid centered className="landingBG">
-          <Grid.Row> <Header> Smart Contract</Header></Grid.Row>
+        <Grid centered>
+          <Grid.Row> <Header inverted> Smart Contract</Header></Grid.Row>
           <Grid.Row columns={3}>
-            <Grid.Column>Distributed</Grid.Column>
-            <Grid.Column> Immutable </Grid.Column>
-            <Grid.Column>Public</Grid.Column>
+            <Grid.Column>
+              <Header inverted>
+              Distributed
+              </Header>
+                <Header as='h5' inverted>
+                  This address book enables any number of users to register and save their
+                  business contacts. You can only see the contacts you have created.
+              </Header>
+            </Grid.Column>
+            <Grid.Column>
+              <Header inverted>
+              Immutable
+              </Header>
+                <Header as='h5' inverted>
+                  This address book enables any number of users to register and save their
+                  business contacts. You can only see the contacts you have created.
+
+              </Header>
+            </Grid.Column>
+            <Grid.Column>
+              <Header inverted>
+              Public
+              </Header>
+              <Header as='h5' inverted>
+                This address book enables any number of users to register and save their
+                business contacts. You can only see the contacts you have created.
+              </Header>
+            </Grid.Column>
           </Grid.Row>
 
         </Grid>
--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Menu, Icon } from 'semantic-ui-react';
+import { Container, Menu, Icon, Header } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
@@ -8,10 +8,10 @@ class Footer extends React.Component {
       <footer>
         <Menu className="secondary">
           <Container>
-            <Menu.Item className="left">AkaMy-Rent</Menu.Item>
-            <Menu.Item ><Icon className="twitter"/></Menu.Item>
-            <Menu.Item ><Icon className="facebook f"/></Menu.Item>
-            <Menu.Item className="right">Contact Us</Menu.Item>
+            <Menu.Item className="left"><Header as='h3'>AkaMy-Rent</Header></Menu.Item>
+            <Menu.Item ><Icon className="twitter" size='big'/></Menu.Item>
+            <Menu.Item ><Icon className="facebook f" size='big'/></Menu.Item>
+            <Menu.Item className="right"><Header as='h3'>Contact Us</Header></Menu.Item>
           </Container>
         </Menu>
       </footer>
