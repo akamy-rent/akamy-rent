3169b62f9192c3d983973590283d4e5963ab6840
Devin Eng <89666809+devineng@users.noreply.github.com>
2022-04-16 16:42:17 -1000

    Modified add smart contract page, added list smart contract page for tenets and admins (#31)
    We'll see about the admin thing.

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,15 +1,18 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Members } from '../../api/member/Member.js';
 
-function addData(data) {
+/* eslint-disable no-console */
+
+// Initialize the database with a default data document.
+function addMember(data) {
   console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
+  Members.collection.insert(data);
 }
 
-// Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
+// Initialize the MembersCollection if empty.
+if (Members.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
-    Meteor.settings.defaultData.map(data => addData(data));
+    Meteor.settings.defaultData.map(data => addMember(data));
   }
 }
---
+++ app/imports/ui/components/MemberItem.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Table } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter, Link } from 'react-router-dom';
+
+/** Renders a single row in the List member table. See pages/Listmember.jsx. */
+class MemberItem extends React.Component {
+  render() {
+    return (
+      <Table.Row>
+        <Table.Cell>{this.props.member.name}</Table.Cell>
+        <Table.Cell>{this.props.member.role}</Table.Cell>
+        <Table.Cell>{this.props.member.stance}</Table.Cell>
+        <Table.Cell>
+          <Link to={`/edit/${this.props.member._id}`}>Edit</Link>
+        </Table.Cell>
+      </Table.Row>
+    );
+  }
+}
+
+// Require a document to be passed to this component.
+MemberItem.propTypes = {
+  member: PropTypes.shape({
+    name: PropTypes.string,
+    role: PropTypes.string,
+    stance: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
+};
+
+// Wrap this component in withRouter since we use the <Link> React Router element.
+export default withRouter(MemberItem);
