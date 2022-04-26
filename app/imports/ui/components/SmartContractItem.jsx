493f3a213d779232aa8ec106694c9ac002334bd2
balemayehu1 <97714392+balemayehu1@users.noreply.github.com>
2022-04-26 00:06:02 -1000

    es lint correction

--- app/imports/ui/components/SmartContractItem.jsx
+++ app/imports/ui/components/SmartContractItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Table } from 'semantic-ui-react';
+import { Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 
